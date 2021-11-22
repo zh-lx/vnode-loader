3ddc8666bdb9d38a630f60a039a5c15bd1c73d14
zhoulixiang <18366276315@163.com>
2021-11-22 17:05:53 +0800

    feat: 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,14 @@ import { parse } from '@vue/compiler-sfc';
 import { LoaderContext } from 'webpack';
 import { getInjectContent } from './inject-ast';
 
-let FileMap = {}; // 全局的FileMap，记录那些文件被处理过
-
 /**
  * @description inject line、column and path to VNode when webpack compiling .vue file
  * @type webpack.loader.Loader
  */
-function TrackCodeLoader(
-  this: LoaderContext<any>,
-  content: string,
-  map: object,
-  meta: any
-) {
+function TrackCodeLoader(this: LoaderContext<any>, content: string) {
   const filePath = this.resourcePath; // 当前文件的绝对路径
-  if (!FileMap[filePath]) {
+  let params = new URLSearchParams(this.resource);
+  if (params.get('type') === 'template') {
     const vueParserContent = parse(content); // vue文件parse后的内容
     const domAst = vueParserContent.descriptor.template.ast; // template开始的dom ast结构
     const templateSource = domAst.loc.source; // template部分的原字符串
@@ -25,10 +19,10 @@ function TrackCodeLoader(
       filePath
     ); // 注入后的template部分字符串
     const newContent = content.replace(templateSource, newTemplateSource);
-    FileMap[filePath] = newContent;
+    return newContent;
+  } else {
+    return content;
   }
-  this.callback(null, FileMap[filePath]);
-  return;
 }
 
 export = TrackCodeLoader;
