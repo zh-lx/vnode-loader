164fcc47eaca33f490283a583f01f40959cfa152
zh-lx <18366276315@163.com>
2021-11-22 22:10:24 +0800

    feat: 1.0.0

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,5 +3,5 @@ import { LoaderContext } from 'webpack';
  * @description inject line、column and path to VNode when webpack compiling .vue file
  * @type webpack.loader.Loader
  */
-declare function TrackCodeLoader(this: LoaderContext<any>, content: string, map: object, meta: any): void;
+declare function TrackCodeLoader(this: LoaderContext<any>, content: string): string;
 export = TrackCodeLoader;
